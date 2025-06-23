36883607fe432f6d655df84d0a26a80a665dc5d4
jacquikeating <j.keating@live.ca>
2025-06-23 18:47:34 -0400

    Update index.js
    - Uncommented ulEl declaration.
    - Uncommented render() function.
    - Updated onValue() function to call render() with the data fetched from Firebase. This allows leads to automatically be displayed on the page whenever changes are made.

--- index.js
+++ index.js
@@ -10,27 +10,29 @@ const database = getDatabase(app);
 const referenceInDB = ref(database, "leads");
 
 onValue(referenceInDB, function (snapshot) {
-  console.log(snapshot.val());
+  const snapshotValues = snapshot.val();
+  const leads = Object.values(snapshotValues);
+  render(leads);
 });
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-// const ulEl = document.getElementById("ul-el");
+const ulEl = document.getElementById("ul-el");
 // const deleteBtn = document.getElementById("delete-btn");
 
-// function render(leads) {
-//   let listItems = "";
-//   for (let i = 0; i < leads.length; i++) {
-//     listItems += `
-//             <li>
-//                 <a target='_blank' href='${leads[i]}'>
-//                     ${leads[i]}
-//                 </a>
-//             </li>
-//         `;
-//   }
-//   ulEl.innerHTML = listItems;
-// }
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
 
 // deleteBtn.addEventListener("dblclick", function () {
 // });
